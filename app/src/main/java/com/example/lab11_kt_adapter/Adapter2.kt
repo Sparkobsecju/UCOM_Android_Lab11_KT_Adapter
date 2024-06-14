895213e5865677e9a80a5b3b3c0170fab18ae9e9
Sivan Chen <ryan1825114@gmail.com>
2024-06-14 10:32:48 +0800

    add adapter2 Activity with adapter2.xml layout

---
+++ app/src/main/java/com/example/lab11_kt_adapter/Adapter2.kt
@@ -0,0 +1,11 @@
+package com.example.lab11_kt_adapter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Adapter2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.adapter2)
+    }
+}
