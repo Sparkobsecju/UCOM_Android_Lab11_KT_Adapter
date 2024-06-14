0facde943272ebfe53e7e139cd77189e690dba6b
Sivan Chen <ryan1825114@gmail.com>
2024-06-14 10:20:06 +0800

    handle context of each data in Adapter with menuInfo: ContextMenu.ContextMenuInfo  -> AdapterContextMenuIndo

--- app/src/main/java/com/example/lab11_kt_adapter/Adapter1.kt
+++ app/src/main/java/com/example/lab11_kt_adapter/Adapter1.kt
@@ -4,13 +4,30 @@ import android.app.ListActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 
+import android.app.ListActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.View
+import android.widget.AdapterView.AdapterContextMenuInfo
+import android.widget.ArrayAdapter
+
 class Adapter1 : ListActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val incomePlaceHolder = resources.getStringArray(R.array.income_placeholder)
-        listAdapter =
-            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, incomePlaceHolder)
-        listView.setOnItemClickListener(Adapter1ItemClickAction (this))
+        listAdapter = ArrayAdapter(this, R.layout.income_item, incomePlaceHolder)
+        listView.setOnItemClickListener(Adapter1ItemClickAction(this))
+        registerForContextMenu(listView)
     }
 
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        val i: AdapterContextMenuInfo = menuInfo as AdapterContextMenuInfo
+        menu!!.add("menu1")
+        menu.setHeaderTitle("第${i.position}個項目長按後的本文選單")
+        super.onCreateContextMenu(menu, v, menuInfo)
+    }
 }
